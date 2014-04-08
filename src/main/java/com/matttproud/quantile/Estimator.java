6ecefd0f41307f8aafffe0b1f6f84e017a11d5e9
Matt T. Proud <matt.proud@gmail.com>
2014-04-08 12:16:03 +0200

    Parameterize buffer and support batch inserts.
    The initial buffer may be too small for certain applications, so it has
    been both grown and enabled for parameterization.
    
    The API now supports batch inserts in a memory efficient manner.

--- src/main/java/com/matttproud/quantile/Estimator.java
+++ src/main/java/com/matttproud/quantile/Estimator.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2013 Matt T. Proud (matt.proud@gmail.com) Copyright 2012 Andrew
  * Wang (andrew@umbrant.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,6 +18,7 @@
 package com.matttproud.quantile;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -28,34 +29,33 @@ import java.util.ListIterator;
  * for streaming calculation of targeted high-percentile epsilon-approximate
  * quantiles.
  * </p>
- * 
+ *
  * <p>
  * This is a generalization of the earlier work by Greenwald and Khanna (GK),
  * which essentially allows different error bounds on the targeted quantiles,
  * which allows for far more efficient calculation of high-percentiles.
  * </p>
- * 
+ *
  * <p>
  * See: Cormode, Korn, Muthukrishnan, and Srivastava
  * "Effective Computation of Biased Quantiles over Data Streams" in ICDE 2005
  * </p>
- * 
+ *
  * <p>
  * Greenwald and Khanna,
  * "Space-efficient online computation of quantile summaries" in SIGMOD 2001
  * </p>
- * 
+ *
  * <p>
  * This type is <em>not</em> concurrency safe.
  * </p>
  */
 public class Estimator<T extends Number & Comparable<T>> {
-  static final int BUFFER_CAPACITY = 512;
+  private static final int DEFAULT_BUFFER_CAPACITY = 4096;
+  private int bufferCap;
+
   // Total number of items in stream
   int count = 0;
-
-  // Used for tracking incremental compression
-  private int compressIdx = 0;
 
   /**
    * Current list of sampled items, maintained in sorted order with error bounds
@@ -64,7 +64,7 @@ public class Estimator<T extends Number & Comparable<T>> {
   /**
    * Buffers incoming items to be inserted in batch.
    */
-  final ArrayList<T> buffer = new ArrayList<T>(BUFFER_CAPACITY);
+  final ArrayList<T> buffer;
   /**
    * Array of Quantiles that we care about, along with desired error.
    */
@@ -72,32 +72,72 @@ public class Estimator<T extends Number & Comparable<T>> {
 
   /**
    * Create an estimator with the provided invariants for quantile estimation.
+   *
+   * @param quantiles requested quantile invariants.
    */
   public Estimator(final Quantile... quantiles) {
     this.quantiles = quantiles;
+
+    bufferCap = DEFAULT_BUFFER_CAPACITY;
+    buffer = new ArrayList<T>(bufferCap);
   }
+
+    /**
+     * <p>
+     * Create an estimator with default invariants for quantile estimation.
+     * </p>
+     *
+     * <ul>
+     * <li>Median at 5 percent inaccuracy.</li>
+     * <li>99th Percentile at 0.1 percent inaccuracy.</li>
+     * </ul>
+     */
+    public Estimator() {
+        this(new Quantile(0.5, 0.05), new Quantile(0.99, 0.001));
+    }
+
+    /**
+     * <p>
+     * Create an estimator with default invariants for quantile estimation.
+     * </p>
+     *
+     * <ul>
+     * <li>Median at 5 percent inaccuracy.</li>
+     * <li>99th Percentile at 0.1 percent inaccuracy.</li>
+     * </ul>
+     *
+     * @param bufferCap The capacity for the internal observations buffer.
+     */
+    public Estimator(final int bufferCap) {
+        this(bufferCap, new Quantile(0.5, 0.05), new Quantile(0.99, 0.001));
+    }
+
+    /**
+     * <p>
+     * Create an estimator with the provided invariants for quantile estimation.
+     * </p>
+     *
+     * @param quantiles requested quantile invariants.
+     * @param bufferCap The capacity for the internal observations buffer.
+     */
+    public Estimator(final int bufferCap, final Quantile... quantiles) {
+        this.quantiles = quantiles;
+
+        this.bufferCap = bufferCap;
+        buffer = new ArrayList<T>(bufferCap);
+    }
 
   /**
    * <p>
-   * Create an estimator with default invariants for quantile estimation.
-   * </p>
-   * 
-   * <ul>
-   * <li>Median at 5 percent inaccuracy.</li>
-   * <li>99th Percentile at 0.1 percent inaccuracy.</li>
-   * </ul>
-   */
-  public Estimator() {
-    this(new Quantile(0.5, 0.05), new Quantile(0.99, 0.001));
-  }
-
-  /**
    * Specifies the allowable error for this rank, depending on which quantiles
    * are being targeted.
-   * 
+   * </p>
+   *
+   * <p>
    * This is the f(r_i, n) function from the CKMS paper. It's basically how wide
    * the range of this rank can be.
-   * 
+   * </p>
+   *
    * @param rank the index in the list of samples
    */
   private double allowableError(final int rank, final int n) {
@@ -112,20 +152,39 @@ public class Estimator<T extends Number & Comparable<T>> {
   }
 
   /**
+   * <p>
    * Add a new value from the stream.
-   * 
-   * @param v
+   * </p>
+   *
+   * @param v observation to be added.
    */
   public void insert(final T v) {
     buffer.add(buffer.size(), v);
 
-    if (buffer.size() == BUFFER_CAPACITY) {
+    if (buffer.size() == bufferCap) {
       flush();
     }
   }
 
-  private void insertBatch() {
-    if (buffer.size() == 0) {
+  /**
+   * <p>
+   * Add new values from the stream.
+   * </p>
+   *
+   * @param vs observations to be added.
+   */
+  public void insert(final Collection<T> vs) {
+    buffer.addAll(vs);
+
+    if (buffer.size() >= bufferCap) {
+      flush();
+    }
+  }
+
+  private void mergeBuffer() {
+    final int bufSize = buffer.size();
+
+    if (bufSize == 0) {
       return;
     }
 
@@ -140,9 +199,9 @@ public class Estimator<T extends Number & Comparable<T>> {
       count++;
     }
 
-    ListIterator<Item> it = samples.listIterator();
+    final ListIterator<Item> it = samples.listIterator();
     Item item = it.next();
-    for (int i = start; i < buffer.size(); i++) {
+    for (int i = start; i < bufSize; i++) {
       final T v = buffer.get(i);
       while (it.nextIndex() < samples.size() && item.value.compareTo(v) < 0) {
         item = it.next();
@@ -165,6 +224,13 @@ public class Estimator<T extends Number & Comparable<T>> {
       item = newItem;
     }
 
+    // Return the buffer back to its originally allocated size in case the
+    // addition overshot the range to be a good memory citizen.
+    if (bufSize > bufferCap) {
+      buffer.subList(bufferCap, bufSize).clear();
+      buffer.trimToSize();
+    }
+
     buffer.clear();
   }
 
@@ -179,7 +245,6 @@ public class Estimator<T extends Number & Comparable<T>> {
     }
 
     final ListIterator<Item> it = samples.listIterator();
-    int removed = 0;
 
     Item prev = null;
     Item next = it.next();
@@ -195,14 +260,13 @@ public class Estimator<T extends Number & Comparable<T>> {
         it.remove();
         // it.next() is now equal to next, skip it back forward again
         it.next();
-        removed++;
       }
     }
   }
 
   /**
    * Get the estimated value at the specified quantile.
-   * 
+   *
    * @param quantile Queried quantile, e.g. 0.50 or 0.99.
    * @return Estimated value at that quantile.
    */
@@ -235,7 +299,7 @@ public class Estimator<T extends Number & Comparable<T>> {
   }
 
   void flush() {
-    insertBatch();
+    mergeBuffer();
     compress();
   }
 
